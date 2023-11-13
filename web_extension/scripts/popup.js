fa85c137e8473a96b8cdec5c0c7f663915abb324
Brandon Hulston <brandonhulston1@gmail.com>
2023-11-12 20:46:51 -0800

    Options page update

--- web_extension/scripts/popup.js
+++ web_extension/scripts/popup.js
@@ -134,7 +134,7 @@ sendObsidian.addEventListener('click', async function () {
         title: elmTitle.value,
         time: elmDuration.value,
         author: elmAuthor.value,
-        url: url
+        url: `${url}`
         };
     const hideAuthor = document.getElementById('author').checked;
     const hideTime = document.getElementById('time').checked;
